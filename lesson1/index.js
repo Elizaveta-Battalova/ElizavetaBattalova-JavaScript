31b34a68d644530d962bb6c87575d088cb4b97db
Darya Batalava <Darya.Batalava@p97.com>
2021-06-17 13:16:48 +0300

    added while

--- lesson1/index.js
+++ lesson1/index.js
@@ -1,11 +1,14 @@
 let name = prompt("Введите ваше ФИО");
-let age = prompt("Введите ваш возраст");
+
+let age = prompt ("Введите ваш возраст");
+while ( !parseInt(age) ) {
+    alert ('Нужно писать число!');
+    age = prompt ("Введите ваш возраст");
+} 
+
 let gender = confirm("Ваш пол - мужской?");
 
 let genderMessage = GetGenderMessage();
-
-alert(`Ваше имя: ${name}\nВаш возраст: ${age}\nВаш пол: ${genderMessage}` );
-
 
 function GetGenderMessage() {
     if(gender)
@@ -17,3 +20,5 @@ function GetGenderMessage() {
         return "женский";
     }
   }
+
+  alert(`Ваше имя: ${name}\nВаш возраст: ${age}\nВаш пол: ${genderMessage}` );
