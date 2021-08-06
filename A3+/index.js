5eea06f4bdbce9949298deb6fcd944758fc36924
Elizaveta Battalova <elizaveta.battalova@rambler.ru>
2021-08-06 14:03:44 +0300

    updated and fixed A3

--- A3+/index.js
+++ A3+/index.js
@@ -1,12 +1,7 @@
 let isPalindrome = function (string) {
-let stringResult = string.toLowerCase().split(" ").join("");
-let str = string.toLowerCase().split(" ").reverse().join("")
-  if (stringResult == str) {
-    alert(string + " палиндорм !");
-  } else {
-    alert(string + " не палиндорм !");
-  }
-};
+let stringResult = string.trim().split(' ').join('').toLowerCase();
+alert(stringResult === stringResult.split('').reverse().join('') ? `Палиндром` : `Не палиндром`);
+}
 
 document.getElementById("btn").onclick = function () {
   isPalindrome(document.getElementById("txt").value);
