a1101b7cd6018e593f6c6b1dbe182dd8535e6214
Elizaveta Battalova <elizaveta.battalova@rambler.ru>
2021-09-10 18:35:06 +0300

    clear my project

--- RUN_HONEY_RUN/main.js
+++
@@ -1,5 +0,0 @@
-// var canvas = document.getElementById("canvas"),
-//   ctx = canvas.getContext("2d");
-
-//   canvas.width = window.innerWidth;
-//   canvas.height = window.innerHeight;
--- honey_test/index.js
+++ honey_test/index.js
@@ -1,35 +1,57 @@
+function hideButton() {
+  let item = document.getElementById("btn");
+  if (item.style.display == "block") item.style.display = "none";
+  else item.style.display = "block";
+}
+
+
+function start() {
+//анимация меда
 let honey = new Image();
-honey.src = "./111.png";
+honey.src = "./honey.png";
 let counter = 0,
   frame_width = 200,
   frame_height = 200;
-honey.onload = function () {
+  honey.onload = function () {
   let canvas = document.getElementById("myCanvas");
   let context = canvas.getContext("2d");
   window.requestAnimationFrame(animate);
 
   function animate() {
     let frame = Math.floor(counter % 6);
-    context.clearRect(0,0,200,200);
-    context.drawImage(
-      honey,
-      frame * frame_width,
-      0,
-      frame_width,
-      frame_height,
-      0,
-      0,
-      frame_width,
-      frame_height
+    context.clearRect(0, 0, 0, 0);
+    context.drawImage(honey,frame * frame_width,0,frame_width,frame_height,0,400,frame_width,frame_height
     );
     counter = counter + 0.25;
-
     window.requestAnimationFrame(animate);
   }
 };
 
-// function keyPressed() {
-//   if (key == " ") {
-//     honey.jump();
-//   }
-// }
+
+
+//анимация фона
+
+let canvas = document.getElementById("myCanvas");
+let context = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let image = new Image();
+
+image.onload = function () {
+  let x = 0;
+  let width = image.width;
+  let min = 0 - width;
+  let step = 1;
+
+  let loop = function () {
+    context.drawImage(image, x, 0);
+    context.drawImage(image, x + width, 0);
+    x -= step;
+    if (x < min) {
+      x = 0;
+    }
+  };
+  setInterval(loop, 1000 / 600);
+};
+image.src = "./forest.png";
+}
